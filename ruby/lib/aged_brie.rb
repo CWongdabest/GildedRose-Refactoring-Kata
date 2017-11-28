146fe76da753190845faf8c1b4eceb9fe0467c3e
Canace Wong <canacewongtoday@hotmail.com>
2017-11-28 17:45:32 +0000

    added tests for aged brie class

--- ruby/lib/aged_brie.rb
+++ ruby/lib/aged_brie.rb
@@ -8,9 +8,19 @@ class Aged_brie
 
   def update_quality
     @item.sell_in -= 1
-    if @item.quality < 50
-      (@item.sell_in >= 0) ? @item.quality += 1 : @item.quality += 2
-    end
+    @item.quality < 50 ? update_quality_less_than_50 : @item.quality = 50 
   end
+
+private
+
+  def update_quality_less_than_50
+    @item.quality > 48 && @item.sell_in < 0 ? @item.quality = 50 : normal_update
+  end
+
+  def normal_update
+    (@item.sell_in >= 0) ? @item.quality += 1 : @item.quality += 2
+  end
+
+
 
 end
--- ruby/spec/aged_brie_spec.rb
+++ ruby/spec/aged_brie_spec.rb
@@ -9,5 +9,18 @@ describe Aged_brie do
     expect(item.quality).to eq (11)
   end
 
+  it "should increase item quality twice when sell in date is passed" do
+    item = Item.new("Aged Brie", 0, 10)
+    aged_brie = Aged_brie.new(item)
+    aged_brie.update_quality
+    expect(item.quality).to eq (12)
+  end
+
+  it "should not increase item quality to more than 50" do
+    item = Item.new("Aged Brie", 0, 49)
+    aged_brie = Aged_brie.new(item)
+    aged_brie.update_quality
+    expect(item.quality).to eq (50)
+  end
 
 end
