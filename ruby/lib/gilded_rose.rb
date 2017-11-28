d85d933dcb9cb57e418f8774d2e143b0b2685654
Canace Wong <canacewongtoday@hotmail.com>
2017-11-28 17:47:05 +0000

    took away some redundant methods in gilded rose

--- ruby/lib/gilded_rose.rb
+++ ruby/lib/gilded_rose.rb
@@ -55,37 +55,6 @@ class GildedRose
     end
   end
 
-  # def update_quality(items)
-  #   items.each do |item|
-  #     if aged_brie?(item)
-  #       aged_brie = Aged_brie.new(item)
-  #       aged_brie.update_quality
-  #     end
-  #   end
-  # end
-
-  def update_brie_quality(item)
-    item.sell_in -= 1
-    if item.quality < 50
-      (item.sell_in >= 0) ? item.quality += 1 : item.quality += 2
-    end
-  end
-
-  def update_backstage_passes_quality(item)
-    item.sell_in -= 1
-      if item.sell_in >= 0
-        if item.sell_in > 11
-          item.quality += 1
-        elsif item.sell_in > 5 && item.sell_in < 11
-          item.quality += 2
-        else
-          item.quality += 3
-        end
-      else
-        item.quality = 0
-      end
-  end
-
   def normal_item(item)
     item.sell_in -= 1
     item.quality -= 1
