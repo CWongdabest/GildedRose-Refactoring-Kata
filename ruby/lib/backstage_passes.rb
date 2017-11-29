ad43b0a29edfa80d0d1db3538810b7fb85f1bfde
Canace Wong <canacewongtoday@hotmail.com>
2017-11-29 12:44:42 +0000

    all backstage tests passes, some refactoring

--- ruby/lib/backstage_passes.rb
+++ ruby/lib/backstage_passes.rb
@@ -6,7 +6,8 @@ class Backstage_passes
 #
   def update_quality
     @item.sell_in -= 1
-    sell_in_date_more_than_11? ? @item.quality += 1 : update_less_than_11
+    sell_in_date_more_than_11? ? normal_update : update_less_than_11
+    set_maximum
   end
 
 private
@@ -15,19 +16,26 @@ private
     @item.sell_in > 10
   end
 
+  def normal_update
+    @item.quality += 1
+  end
+
   def update_less_than_11
+    @item.sell_in < 1 ? update_less_than_1 : update_between_0_and_10
+  end
+
+  def update_less_than_1
+    @item.quality = 0
+  end
+
+  def update_between_0_and_10
     @item.sell_in > 5 ? @item.quality += 2 : @item.quality +=3
   end
-#
-#   def normal_update
-#     (@item.sell_in >= 0) ? @item.quality += 1 : @item.quality += 2
-#     set_maximum
-#   end
-#
-#   def set_maximum
-#     if @item.quality > 50
-#       @item.quality = 50
-#     end
-#   end
+
+  def set_maximum
+    if @item.quality > 50
+      @item.quality = 50
+    end
+  end
 
 end
--- ruby/spec/backstage_passes_spec.rb
+++ ruby/spec/backstage_passes_spec.rb
@@ -16,25 +16,25 @@ describe Backstage_passes do
     expect(item.quality).to eq (12)
   end
   #
-  # it "should increase item quality by three times when sell_by date is less than 5" do
-  #   items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 5, 10)]
-  #   gildedrose = GildedRose.new(items)
-  #   gildedrose.update_quality
-  #   expect(items[0].quality).to eq (13)
-  # end
+  it "should increase item quality by three times when sell_by date is less than 5" do
+    item = Item.new("Backstage passes to a TAFKAL80ETC concert", 5, 10)
+    backstage_passes = Backstage_passes.new(item)
+    backstage_passes.update_quality
+    expect(item.quality).to eq (13)
+  end
   #
-  # it "should not increase item quality to more than 50" do
-  #   items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 5, 50)]
-  #   gildedrose = GildedRose.new(items)
-  #   gildedrose.update_quality
-  #   expect(items[0].quality).to eq (50)
-  # end
-  #
-  # it "item quality should be zero after sell_in date" do
-  #   items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 0, 50)]
-  #   gildedrose = GildedRose.new(items)
-  #   gildedrose.update_quality
-  #   expect(items[0].quality).to eq (0)
-  # end
+  it "should not increase item quality to more than 50" do
+     item = Item.new("Backstage passes to a TAFKAL80ETC concert", 5, 50)
+     backstage_passes = Backstage_passes.new(item)
+     backstage_passes.update_quality
+     expect(item.quality).to eq (50)
+ end
+
+  it "item quality should be zero after sell_in date" do
+    item = Item.new("Backstage passes to a TAFKAL80ETC concert", 1, 50)
+    backstage_passes = Backstage_passes.new(item)
+    backstage_passes.update_quality
+    expect(item.quality).to eq (0)
+  end
 
 end
