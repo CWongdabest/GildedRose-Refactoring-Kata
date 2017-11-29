1d7ab86083c186ffb5bdc0dad41f07fd594a8531
Canace Wong <canacewongtoday@hotmail.com>
2017-11-29 13:11:31 +0000

    stubbed Aged Brie tests

--- ruby/spec/aged_brie_spec.rb
+++ ruby/spec/aged_brie_spec.rb
@@ -2,22 +2,31 @@ require 'aged_brie'
 
 describe Aged_brie do
 
+  class FakeItem
+      attr_accessor :sell_in, :quality
+
+      def initialize(name, sell_in, quality)
+        @sell_in = sell_in
+        @quality = quality
+      end
+  end
+
   it "should increase item quality when sell in date decreases" do
-    item = Item.new("Aged Brie", 10, 10)
+    item = FakeItem.new("Aged Brie", 10, 10)
     aged_brie = Aged_brie.new(item)
     aged_brie.update_quality
     expect(item.quality).to eq (11)
   end
 
   it "should increase item quality twice when sell in date is passed" do
-    item = Item.new("Aged Brie", 0, 10)
+    item = FakeItem.new("Aged Brie", 0, 10)
     aged_brie = Aged_brie.new(item)
     aged_brie.update_quality
     expect(item.quality).to eq (12)
   end
 
   it "should not increase item quality to more than 50" do
-    item = Item.new("Aged Brie", 0, 49)
+    item = FakeItem.new("Aged Brie", 0, 49)
     aged_brie = Aged_brie.new(item)
     aged_brie.update_quality
     expect(item.quality).to eq (50)
